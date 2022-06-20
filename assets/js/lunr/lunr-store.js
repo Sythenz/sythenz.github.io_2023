6f478ffc302e3a0c50d0799f04b9c2e63d5cf576
Sythenz <alessafur@protonmail.com>
2022-06-20 23:26:59 +0200

    Pushing Codekitten.me

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Anatomy of a Widget",
+        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": [],
+        "tags": [],
+        "url": "/Anatomy-Of-A-Widget/",
+        "teaser": null
+      }]
