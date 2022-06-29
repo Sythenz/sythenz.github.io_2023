6c6d8bdf6f9cd97b66a08ce884ceeac2fb291f9a
Sythenz <alessafur@protonmail.com>
2022-06-30 01:30:13 +0200

    Redesign and Anatomy of a Widget article.

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
         "title": "Anatomy of a Widget",
-        "excerpt":"Welcome   Hello world, this is my first Jekyll blog post.   I hope you like it!  ","categories": [],
+        "excerpt":"Welcome So! For the sake of future tutorials I plan to write here, I thought it be best to do a quick write up on creating a widget for Slate in C++ with Unreal Engine. I will link back to this then from other tutorials for the sake of time....","categories": [],
         "tags": [],
         "url": "/Anatomy-Of-A-Widget/",
         "teaser": null
