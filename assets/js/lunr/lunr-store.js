0fca66d37f32df1bf4bcf16921f1d2b71fb6a834
Sythenz <alessafur@protonmail.com>
2022-06-30 10:17:45 +0200

    Submitting articles

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,7 +1,25 @@
 var store = [{
+        "title": "Custom Details Panels in Unreal Engine (FPropertyEditorModule)",
+        "excerpt":"In Unreal Engine, we can create our own Custom Details panels relatively easily by using the FPropertyEditorModule and passing it a custom class with listed UProperties instead of having to draw and generate our own Slate to display them. For this project, we are going to use UE4.24 but you...","categories": [],
+        "tags": [],
+        "url": "/Custom-Details-Panels/",
+        "teaser": null
+      },{
         "title": "Anatomy of a Widget",
-        "excerpt":"Welcome So! For the sake of future tutorials I plan to write here, I thought it be best to do a quick write up on creating a widget for Slate in C++ with Unreal Engine. I will link back to this then from other tutorials for the sake of time....","categories": [],
+        "excerpt":"So! For the sake of future tutorials I plan to write here, I thought it be best to do a quick write up on creating a widget for Slate in C++ with Unreal Engine. I will link back to this then from other tutorials for the sake of time. To...","categories": [],
         "tags": [],
         "url": "/Anatomy-Of-A-Widget/",
+        "teaser": null
+      },{
+        "title": "Notifications in Unreal Engine (Part 1 — Starting Point)",
+        "excerpt":"In this tutorial, we’re going to cover creating notifications in Unreal Engine, similar to the “Lighting Build Completed” dialog boxes, or whenever Unreal asks something in the bottom right corner. Here’s an example of what we’re going to be writing over the course of three parts, the top notification style...","categories": [],
+        "tags": [],
+        "url": "/Notifications-In-Unreal-Engine-Part1/",
+        "teaser": null
+      },{
+        "title": "Notifications in Unreal Engine (Part 2 — Hyperlinks and Styling)",
+        "excerpt":"In this part, we’re going to cover Hyperlinks which go a little more in depth to methods we can pass into our FNotificationInfo. We will also cover some cool styling things we can do with icons on our notification and briefly touch on Styling slate if you would like to...","categories": [],
+        "tags": [],
+        "url": "/Notifications-In-Unreal-Engine-Part-2/",
         "teaser": null
       }]
