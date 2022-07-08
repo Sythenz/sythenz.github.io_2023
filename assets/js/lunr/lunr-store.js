cec8be5477fc0f3b05a183dc381ad368347b8c28
Sythenz <alessafur@protonmail.com>
2022-07-08 16:37:06 +0200

    New config

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,67 @@
+var store = [{
+        "title": "How To Lower Your Unreal Engine Project File Size!",
+        "excerpt":"Want to learn how to lower and optimize your Unreal Engine project file sizes? During one of my Twitch streams we investigated this. Here’s what we found!  ","categories": ["Unreal"],
+        "tags": ["Unreal Engine","Video"],
+        "url": "/unreal/Lower-Project-Size/",
+        "teaser": "https://i.ytimg.com/vi/gmYvSPj8rmI/maxresdefault.jpg"
+      },{
+        "title": "Writing HLSL Code OUTSIDE The Custom Node in UE4",
+        "excerpt":"Learn how to create your own external Shader directory to store Unreal Shader Headers (HLSL) files, instead of using that pesky tiny custom node text box! :3  ","categories": ["Unreal"],
+        "tags": ["Unreal Engine","Video"],
+        "url": "/unreal/Custom-Node-In-UE4/",
+        "teaser": "https://i.ytimg.com/vi/V3BVsYV7ge0/maxresdefault.jpg"
+      },{
+        "title": "Case Study: Organizing Your Materials in Unreal Engine 4!",
+        "excerpt":"In this tutorial, we will go through the prototype version of our Stylized Gradient material and take a Tech Artist approach to organizing and keeping our material graph clean so that understanding our Material Graph becomes easier for those who work with it in the future!  ","categories": ["Unreal"],
+        "tags": ["Unreal Engine","Video"],
+        "url": "/unreal/Organizing-Your-Materials/",
+        "teaser": "https://i.ytimg.com/vi/dyvgUpexxy0/maxresdefault.jpg"
+      },{
+        "title": "Custom Details Panels in Unreal Engine (FPropertyEditorModule)",
+        "excerpt":"In Unreal Engine, we can create our own Custom Details panels relatively easily by using the FPropertyEditorModule and passing it a custom class with listed UProperties instead of having to draw and generate our own Slate to display them. For this project, we are going to use UE4.24 but you...","categories": ["Unreal"],
+        "tags": ["Unreal Engine","C++","Plugin Development","Written"],
+        "url": "/unreal/Custom-Details-Panels/",
+        "teaser": "/assets/images/teaser_customdetails.png"
+      },{
+        "title": "Anatomy of a Widget",
+        "excerpt":"So! For the sake of future tutorials I plan to write here, I thought it be best to do a quick write up on creating a widget for Slate in C++ with Unreal Engine. I will link back to this then from other tutorials for the sake of time. To...","categories": ["Unreal"],
+        "tags": ["Unreal Engine","Written","C++","Plugin Development"],
+        "url": "/unreal/Anatomy-Of-A-Widget/",
+        "teaser": "/assets/images/teaser_anatomy.png"
+      },{
+        "title": "Desert Bus VR (2017)",
+        "excerpt":"I only joined this team for two short weeks but the team were amazing and I had a lot of fun working on such a silly and amazing project. I grew up watching Pen &amp; Teller, so to work on a title created by them in the late 1990’s was...","categories": ["Portfolio"],
+        "tags": [],
+        "url": "/portfolio/Desert-Bus-VR/",
+        "teaser": "/assets/images/DesertBus.png"
+      },{
+        "title": "The Lunar Effect (Selene)",
+        "excerpt":"Selene is a lighting rendering pass built with the purpose of adding Vertex Lighting functionality back to Unreal Engine. I met The Lunar Effect team member Louie Woodhouse in April of 2020, in the Haunted PS1 community and began working with them on creating Selene and other Shaders, and technical...","categories": ["Portfolio"],
+        "tags": [],
+        "url": "/portfolio/Selene-The-Lunar-Effect/",
+        "teaser": "/assets/images/thelunareffect.png"
+      },{
+        "title": "Notifications in Unreal Engine (Part 1 — Starting Point)",
+        "excerpt":"In this tutorial, we’re going to cover creating notifications in Unreal Engine, similar to the “Lighting Build Completed” dialog boxes, or whenever Unreal asks something in the bottom right corner. Here’s an example of what we’re going to be writing over the course of three parts, the top notification style...","categories": ["Unreal"],
+        "tags": ["Unreal Engine","C++","Written","Plugin Development"],
+        "url": "/unreal/Notifications-In-Unreal-Engine-Part1/",
+        "teaser": "/assets/images/teaser_notifications_part1.png"
+      },{
+        "title": "Notifications in Unreal Engine (Part 2 — Hyperlinks and Styling)",
+        "excerpt":"In this part, we’re going to cover Hyperlinks which go a little more in depth to methods we can pass into our FNotificationInfo. We will also cover some cool styling things we can do with icons on our notification and briefly touch on Styling slate if you would like to...","categories": ["Unreal"],
+        "tags": ["Unreal Engine","C++","Written","Plugin Development"],
+        "url": "/unreal/Notifications-In-Unreal-Engine-Part-2/",
+        "teaser": "/assets/images/teaser_notifications_part2.png"
+      },{
+        "title": "Quixel (2015-2017)",
+        "excerpt":"I began working with Quixel very early in my career, Teddy allowed me early testing of nDO and dDO back in the day when I was an Environment Artist. As Quixel grew over the years I joined them to assist and as my skillsets grew in Technical Art - I...","categories": ["Portfolio"],
+        "tags": [],
+        "url": "/portfolio/Quixel/",
+        "teaser": "/assets/images/quixel.png"
+      },{
+        "title": "Chorus (2021)",
+        "excerpt":"Responsibilities Creating tools for optimization and performance analysis both in Unreal (Slate &amp; C++), Python, and Mysql (Grafana) Creating tools to assist artists with optimization pipelines, Quality-Of-Life engine features, LOD Generation Tools etc (Python, Slate &amp; C++). Resident Shader Witch 🧹 working for Visual Effects, Cinematics and gameplay elements. Everything...","categories": ["Portfolio"],
+        "tags": [],
+        "url": "/portfolio/Chorus/",
+        "teaser": "https://chorusthegame.com/media/KeyArtv2/CHORVS_FIN_HORIZ_093020-2560.jpg"
+      }]
